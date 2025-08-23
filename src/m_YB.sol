82a622a3356a880a65fe14cb4cc0ba23c092effc
jtdoherty <jtdoherty1105@gmail.com>
2025-08-23 16:49:58 -0400

    asjusted token formatting

--- src/m_YB.sol
+++ src/m_YB.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract m_YB is ERC20, Ownable {
     constructor(address initialOwner)
--- src/m_ybBTC.sol
+++ src/m_ybBTC.sol
@@ -5,7 +5,10 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract m_ybBTC is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("Equilibrium ybBTC", "m-ybBTC") Ownable(initialOwner) {}
+    constructor(address initialOwner)
+        ERC20("Equilibrium ybBTC", "m-ybBTC")
+        Ownable(initialOwner)
+    {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
